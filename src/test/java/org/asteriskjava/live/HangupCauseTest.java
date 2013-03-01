0a0be5fdd9900f62d5af5b90a7f558172dc4e32a
Alexandre Moutot <a.moutot@alphalink.fr>
2013-03-01 14:19:38 +0100

    POM update and test to validate new HangupCause

--- src/test/java/org/asteriskjava/live/HangupCauseTest.java
+++ src/test/java/org/asteriskjava/live/HangupCauseTest.java
@@ -11,4 +11,14 @@ public class HangupCauseTest
     {
         assertEquals("Valid enum for cause code 18", HangupCause.AST_CAUSE_NO_USER_RESPONSE, HangupCause.getByCode(18));
     }
+
+    @Test
+    public void testNewCause()
+    {
+        assertEquals("Valid enum for cause code 5", HangupCause.AST_CAUSE_MISDIALLED_TRUNK_PREFIX, HangupCause.getByCode(5));
+        assertEquals("Valid enum for cause code 14", HangupCause.AST_CAUSE_NUMBER_PORTED_NOT_HERE, HangupCause.getByCode(14));
+        assertEquals("Valid enum for cause code 20", HangupCause.AST_CAUSE_SUBSCRIBER_ABSENT, HangupCause.getByCode(20));
+        assertEquals("Valid enum for cause code 23", HangupCause.AST_CAUSE_REDIRECTED_TO_NEW_DESTINATION, HangupCause.getByCode(23));
+        assertEquals("Valid enum for cause code 26", HangupCause.AST_CAUSE_ANSWERED_ELSEWHERE, HangupCause.getByCode(26));
+    }
 }
